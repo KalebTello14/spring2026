5a0e6ac08a655ace80725db80e524cf1183c65e9
KalebTello14 <156832840+KalebTello14@users.noreply.github.com>
2026-04-23 16:25:04 +0000

    Stop tracking folder listed in .gitignore

--- homework6/target/debug/deps/homework6-07eb913840339598.d
+++
@@ -1,5 +0,0 @@
-/workspaces/spring2026/homework6/target/debug/deps/homework6-07eb913840339598.d: src/main.rs
-
-/workspaces/spring2026/homework6/target/debug/deps/homework6-07eb913840339598: src/main.rs
-
-src/main.rs:
--- homework6/target/debug/deps/homework6-be7dbf78101c3598.d
+++
@@ -1,5 +0,0 @@
-/workspaces/spring2026/homework6/target/debug/deps/homework6-be7dbf78101c3598.d: src/main.rs
-
-/workspaces/spring2026/homework6/target/debug/deps/homework6-be7dbf78101c3598: src/main.rs
-
-src/main.rs:
